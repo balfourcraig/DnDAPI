6364b2de37a92c30678d9ce29057f70a43c7974f
Craig <jonquil@tutamail.com>
2023-04-08 20:37:49 +1200

    CORS

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,4 +22,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(builder => builder.WithOrigins("https://balfourcraig.github.io").AllowAnyHeader().AllowAnyMethod());
 app.Run();
