75f5250b06b3af58f4607f3eabfe8435828630bd
Craig <jonquil@tutamail.com>
2025-12-12 17:52:04 +1300

    Restructure as static site and remove AI crap

--- wwwroot/scripts/beyond/classes/class.js
+++
@@ -1,8 +0,0 @@
-function createClassHolder(title){
-	const holder = document.createElement('div');
-
-	holder.appendChild(element('h3', title));
-
-	holder.appendChild(document.createElement('hr'));
-	return holder;
-}
--- wwwroot/scripts/site.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    updateLinks();
-    const holder = document.getElementById('rawContent');
-    const url = document.getElementById('url');
-    if(holder &&  url && url.innerText !== '') {
-        holder.innerHTML = '';
-        fetchDnDData(url.innerText, holder)
-    }
-});
